123d14f5fdb302ee405e9a56d31c269171c1b600
techy <76910214+awesomeTechYug@users.noreply.github.com>
2023-11-23 11:56:44 +0545

    forbid voting after 5 min
    forbid voting and revert the transaction after the
    Deadline - 5 min or 300sec

---
+++ Ballot.sol
@@ -0,0 +1,124 @@
+// SPDX-License-Identifier:GPL-3.0
+pragma solidity ^0.8.5;
+contract Ballot {
+    
+    uint startTime; // timestamp added
+
+    struct Voter {
+        uint weight; 
+        bool voted;  
+        address delegate; 
+        uint vote;  
+    }
+
+    struct Proposal {
+        bytes32 name; 
+        uint voteCount; 
+    }
+
+    address public chairperson;
+//Deploy: ["0x666f6f0000000000000000000000000000000000000000000000000000000000","0x6261720000000000000000000000000000000000000000000000000000000000"]
+// voters
+// 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2
+// 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db
+// 0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB
+
+
+    mapping(address => Voter) public voters;
+
+    Proposal[] public proposals;
+
+    constructor(bytes32[] memory proposalNames) {
+        chairperson = msg.sender;
+        voters[chairperson].weight = 1;
+        startTime = block.timestamp;  //initialize statTime
+
+     
+        for (uint i = 0; i < proposalNames.length; i++) {
+           
+            proposals.push(Proposal({
+                name: proposalNames[i],
+                voteCount: 0
+            }));
+        }
+    }
+
+    function giveRightToVote(address voter) external {
+     
+        require(
+            msg.sender == chairperson,
+            "Only chairperson can give right to vote."
+        );
+        require(
+            !voters[voter].voted,
+            "The voter already voted."
+        );
+        require(voters[voter].weight == 0);
+        voters[voter].weight = 1;
+    }
+
+    function delegate(address to) external {
+        // assigns reference
+        Voter storage sender = voters[msg.sender];
+        require(!sender.voted, "You already voted.");
+
+        require(to != msg.sender, "Self-delegation is disallowed.");
+
+        while (voters[to].delegate != address(0)) {
+            to = voters[to].delegate;
+
+            require(to != msg.sender, "Found loop in delegation.");
+        }
+
+  
+        sender.voted = true;
+        sender.delegate = to;
+        Voter storage delegate_ = voters[to];
+        if (delegate_.voted) {
+        
+            proposals[delegate_.vote].voteCount += sender.weight;
+        } else {
+         
+            delegate_.weight += sender.weight;
+        }
+    }
+
+
+//modifier to check if voted within voting timeperiod
+    modifier voteEnded() {
+    require(block.timestamp <= startTime + 300 seconds, "Voting period has ended");
+    _;
+}
+
+    
+    function vote(uint proposal) external voteEnded {
+        Voter storage sender = voters[msg.sender];
+        require(sender.weight != 0, "Has no right to vote");
+        require(!sender.voted, "Already voted.");
+        sender.voted = true;
+        sender.vote = proposal;
+
+     
+        proposals[proposal].voteCount += sender.weight;
+    }
+
+ 
+    function winningProposal() public view
+            returns (uint winningProposal_)
+    {
+        uint winningVoteCount = 0;
+        for (uint p = 0; p < proposals.length; p++) {
+            if (proposals[p].voteCount > winningVoteCount) {
+                winningVoteCount = proposals[p].voteCount;
+                winningProposal_ = p;
+            }
+        }
+    }
+
+
+    function winnerName() external view
+            returns (bytes32 winnerName_)
+    {
+        winnerName_ = proposals[winningProposal()].name;
+    }
+}
