ab78b6d57ffaa1fe0498c93ce143f0911cb79686
techy <76910214+awesomeTechYug@users.noreply.github.com>
2023-11-23 09:42:34 +0545

    Update HelloWorld.sol

--- HelloWorld.sol
+++ HelloWorld.sol
@@ -1,13 +1,17 @@
 // SPDX-License-Identifier: Pratik
 pragma solidity ^0.8.0;
 
+// HelloWord contract created
 contract HelloWorld {
+// Declared a private variable called `storedNumber` to store an unsigned integer
     uint256 private storedNumber;
 
+// function stores unsigned integers in the `storedNumber` variable
     function storeNumber(uint256 number) public {
         storedNumber = number;
     }
 
+//returns the value of the `storedNumber` variable
     function retrieveNumber() public view returns (uint256) {
         return storedNumber;
     }
